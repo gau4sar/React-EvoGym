b014453d43223944c920cc84d8398b7238cab954
GST <gaurav4sarma@gmail.com>
2023-05-22 11:48:45 +0530

    Benefits section is created

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -86,6 +86,19 @@ const Home = ({ setSelectedPage }: Props) => {
           <img alt="home-pageGraphic" src={HomePageGraphic} />
         </div>
       </motion.div>
+
+      {/* SPONSORS */}
+      {isAboveMediumScreens && (
+        <div className="h-[150px] w-full bg-primary-100 py-10">
+          <div className="mx-auto w-5/6">
+            <div className="flex w-3/5 items-center justify-between gap-8">
+              <img alt="redbull-sponsor" src={SponsorRedBull} />
+              <img alt="forbes-sponsor" src={SponsorForbes} />
+              <img alt="fortune-sponsor" src={SponsorFortune} />
+            </div>
+          </div>
+        </div>
+      )}
     </section>
   );
 };
